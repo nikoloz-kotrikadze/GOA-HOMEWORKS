1b9e1933fc88c71d6fab237275aa645f49d05d07
nikoloz-kotrikadze <kotrikadzenikoloz091@gmail.com>
2024-03-19 20:53:14 +0400

    second commit

---
+++ day 6/lesson 6.py/homework 6.py
@@ -0,0 +1,25 @@
+print(10>3)
+print(40<40.1)
+print(8>1.11111111111)
+print(3<8)
+print(4.03333333>4.1)
+
+print(2<3 and 3>2)
+print(9>2 and 2<9)
+print(6<9 and 9>6)
+print(3.2>3 and 3<3.2)
+print(9.123456<9.2 and 9.2>9.123456)
+
+print(30<39 and 39>30) #true
+print(9>5 or 9<5) #true
+
+print(input("your age"))
+your_age=23
+print(input("random number"))
+random_number=13
+print(23>13 or 23<13)
+print(23>13 and 13<23)
+
+print(20>19)
+print(19<20)
+print(20>19 and 19<20)
