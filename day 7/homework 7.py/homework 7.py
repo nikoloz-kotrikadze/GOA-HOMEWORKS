a825d62bfc63f0b80b6002a4c5beb6dce037997c
nikoloz-kotrikadze <kotrikadzenikoloz091@gmail.com>
2024-03-22 20:47:23 +0400

    second commit

---
+++ day 7/homework 7.py/homework 7.py
@@ -0,0 +1,16 @@
+print(input("your name")) #nika
+print(input("your last name")) #kotrikadze
+print(input("your age")) #13
+print(input("your hight")) #1.54m
+
+print(2>1 and 5<7)
+print(6<4 and 8>3)
+print(9>3 and 4<6)
+print(3<5 and 7>4)
+print(5<8 and 2>1)
+
+print(36<45 or 45<36)
+print(76>34 or 34>76) 
+print(99<100 or 100<99)
+print(24>23 or 23>24)
+print(11<56 or 56<11)
